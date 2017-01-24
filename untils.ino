96da3f03aff017107da64d9c17474124f0ea7300
Peter Darling <pete.darling@gmail.com>
2017-01-24 21:29:45 +0000

    Forgot the until file

---
+++ untils.ino
@@ -0,0 +1,31 @@
+/**
+  Return a value from within a CSV string where index is the coloumn count,
+  is zero based, 0=1, 1=2 and so on...
+*/
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {
+    0, -1
+  };
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+/**
+ * double version of map function
+ */
+double mapDouble(double x, double in_min, double in_max, double out_min, double out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
